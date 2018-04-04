0c19d1dce9366719b89e0796bded103ebc0bf5de
Reid <Reid@pokersprout.com>
2018-04-04 12:39:26 -0400

    Experimenting with helper.js and Pug templating rules
    Experimenting with helper.js and Pug templating rules to pass data onto
    res.locals in different ways: for all routes and for specific routes,
    also so that in template calculations using JS libraries are a bit less
    cumbersome thanks to some helper file work and to requiring the
    helper.js file in the middleware file, app.js.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.render('hello', {
+    name: "Liebe",
+    dog: req.query.dog,
+    title: `Here's the title!`
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // if(req.params.name){
+  //   let name = (req.params.name).reverse();
+  //   res.send(name);
+  // }
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
