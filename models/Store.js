9d28ceed5e72f1d68ffc9f8b2ccd0e72a3d43b49
Reid <Reid@pokersprout.com>
2018-04-06 14:18:44 -0400

    Built first database model and schema with mongoose
    Built first database model and schema with mongoose in Store.js using a
    ‘pre’ save that sets the name of the slug equal to the name of current
    store.

---
+++ models/Store.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+  this.slug = slug(this.name);
+});
+
+module.exports = mongoose.model('Store', storeSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+
 
 // Do work here
-router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
-  res.render('hello', {
-    name: "Liebe",
-    dog: req.query.dog,
-    title: `Here's the title!`
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  // if(req.params.name){
-  //   let name = (req.params.name).reverse();
-  //   res.send(name);
-  // }
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
