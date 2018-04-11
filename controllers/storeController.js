35691937c63c00a20ed45f24337ffeb8a07eb661
Reid <Reid@pokersprout.com>
2018-04-11 13:47:19 -0400

    Allow user to edit stores and added edit route
    Allow user to edit stores and added edit route. Require user
    authentication to check if user owns the store.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    res.render('index');
+  res.render('index');
 };
 
 exports.addStore = (req, res) => {
@@ -10,7 +10,33 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created <strong>${store.name}</strong>. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store give the id
+  const store = await Store.findOne({ _id: req.params.id })
+  // 2. Confirm user is the owner of the store (require login)
+    // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+};
+
+exports.updateStore = async (req, res) => {
+  // Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // Returns new store instead of updated store
+    runValidators: true // Make sure that validation is run again, not only on store creation
+  }).exec();
+  // Redirect user to the store and tell them the edit worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href='/stores/${store.slug}'>View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+
 };
